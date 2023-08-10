712e8e62fe3b11ca8e46c9b0a8c099eb49a634ab
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-11 00:16:17 +0100

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(capitalizer)
 }
+
+function capitalizer(tutorials){
+ return tutorials
+ .split(" ")
+ .map(word =>
+    word.charAt(0).toUpperCase() + 
+    word.slice(1))
+    .join(" ")
+ 
+}
+
